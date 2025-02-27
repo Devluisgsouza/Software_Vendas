ac039454236c9be1baad7a594c700b9bad6d592f
DevLGSouza <devlgsouza@gmail.com>
2025-02-26 23:52:04 -0300

    Manipulação de arquivos

--- main/main.py
+++ main/main.py
@@ -1,18 +1,25 @@
 from funcoes.func import desconto
 
+# Cria as variáveis
 totprod = 0
 totdesc = 0
 total = 0
 totproddesc = 0
-while True:
+
+# Loop para adicionar produtos
+while True: 
     produto = float(input('Valor do produto: '))
     totprod += 1
+
+# Verifica se o produto tem desconto, se tiver, aplica o desconto
     if produto >= 100:
         totdesc += 1
         produto = produto - desconto(produto)
         total += produto
     else:
         total += produto
+
+# Pergunta se deseja adicionar mais produtos
     decide = int(input("Adicionar mais produtos? [1] Sim [2] Não: "))
     if decide == 2:
         print(f"Total de produtos: {totprod}")
