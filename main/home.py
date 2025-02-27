071453c69dc5af498f56cef82b98a346b583a757
DevLGSouza <devlgsouza@gmail.com>
2025-02-27 20:09:08 -0300

    correção de arquivos

--- main/home.py
+++ main/home.py
@@ -8,26 +8,25 @@ total = 0
 totproddesc = 0
 
 
-
-
 print("WELCOME TO SALES SYSTEM")
 
 
 # Loop para adicionar produtos
-name = open("main/banco_de_dados/nome.txt", "x")
-price = open("main/banco_de_dados/price.txt", "x")
 
 
 while True:
+    name = open("main/banco_de_dados/name.txt", "a")
     nomeproduto = str(input("Product's name: "))
-    name.write(nomeproduto)
+    name.write(nomeproduto) and name.write("\n")
+    price = open("main/banco_de_dados/price.txt", "a")
     preçoproduto = float(input("Product's price: "))
-    price.write(transfstr(preçoproduto))
+    price.write(transfstr(preçoproduto)) and price.write("\n")
+
     totprod += 1
 # Verifica se o produto tem desconto, se tiver, aplica o desconto
     if preçoproduto >= 100:
         totdesc += 1
-        preçoprodutoproduto = preçoprodutoproduto - desconto(preçoproduto)
+        preçoproduto = preçoproduto - desconto(preçoproduto)
         total += preçoproduto
     else:
         total += preçoproduto
