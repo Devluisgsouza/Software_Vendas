b81f9175474a505c097749abec8e46191c8b57fd
DevLGSouza <devlgsouza@gmail.com>
2025-02-27 23:15:58 -0300

    Criando  conta de usuario

--- main/home.py
+++ main/home.py
@@ -6,23 +6,31 @@ totprod = 0
 totdesc = 0
 total = 0
 totproddesc = 0
+listname = []
+listprice = []
+listlogin = []
+
+open("main/banco_de_dados/login.txt", "r")
+open("main/banco_de_dados/senha.txt", "r")
 
 
-print("WELCOME TO SALES SYSTEM")
+print(
+    "WELCOME TO THE SALE SYSTEM\n\nDO YOU HAVE AN ACCOUNT? \n[1] YES  OR  [2] NO")
 
 
-# Loop para adicionar produtos
-
-
+# Loop para adicionar produtos e preços
 while True:
     name = open("main/banco_de_dados/name.txt", "a")
     nomeproduto = str(input("Product's name: "))
     name.write(nomeproduto) and name.write("\n")
+    listname.append(nomeproduto)
     price = open("main/banco_de_dados/price.txt", "a")
     preçoproduto = float(input("Product's price: "))
     price.write(transfstr(preçoproduto)) and price.write("\n")
-
+    listprice.append(preçoproduto)
     totprod += 1
+
+
 # Verifica se o produto tem desconto, se tiver, aplica o desconto
     if preçoproduto >= 100:
         totdesc += 1
