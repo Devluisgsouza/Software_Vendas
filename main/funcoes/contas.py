3ac7bdcac4dbc52cb6c09820adf334e50f93a487
DevLGSouza <devlgsouza@gmail.com>
2025-02-28 02:11:51 -0300

    login
    termino da parte de login

---
+++ main/funcoes/contas.py
@@ -0,0 +1,22 @@
+def criar_conta():
+    login = input("Login: ")
+    password = input("Password: ")
+    with open("main/banco_de_dados/accounts.txt", "a") as arquivo:
+        arquivo.write(f"{login},{password}\n")
+    print("account created successfully")
+
+
+
+def fazer_login():
+    login = input("Login: ")
+    password = input("Password: ")
+    with open("main/banco_de_dados/accounts.txt", "r") as arquivo:
+        contas = arquivo.readlines()
+    
+    for conta in contas:
+        login_salvo, senha_salva = conta.strip().split(",")
+        if login == login_salvo and password == senha_salva:
+            print("Successfully login")
+            return
+    print("Wrong infomations!")
+    
--- main/home.py
+++ main/home.py
@@ -1,5 +1,6 @@
 from funcoes.desconto import desconto
 from funcoes.transforma import transfstr
+from funcoes.contas import criar_conta, fazer_login
 
 # Criação das variáveis
 totprod = 0
@@ -8,40 +9,45 @@ total = 0
 totproddesc = 0
 listname = []
 listprice = []
-listlogin = []
-listpass = []
-listaccount = []
-
-
-
-login = open("main/banco_de_dados/login.txt", "a")
-account = open("main/banco_de_dados/accounts.txt", "a")
 
 
 print("WELCOME TO THE SALE SYSTEM\n")
 print("DO YOU HAVE AN ACCOUNT?")
 resp = str(input("[1] YES   or   [2] NO\n"))
-if resp != "1":
-    print("CREATE YOUR ACCOUNT\n")
-    log1 = str(input("Login: "))
-    pass1 = str(input("Passwor: "))
-
-    account.write(f"{log1},{pass1}\n")
-
+if resp == "1":
+    fazer_login()
 else:
-    account = open("main/banco_de_dados/accounts.txt", "r")
-    print("ENTER WITH YOUR ACCOUNT\n")
-    log2 = str(input("Login: "))
-    pass2 = str(input("Passwor: "))
-    login.write(f"{log2},{pass2}\n")
-    if login.readline() in account.readlines():
-        print("open")
-account.close()
-login.close()
+    criar_conta()
 
 
+# Loop para adicionar produtos e preços
+while True:
+    name = open("main/banco_de_dados/name.txt", "a")
+    nomeproduto = str(input("Product's name: "))
+    name.write(nomeproduto) and name.write("\n")
+    listname.append(nomeproduto)
+    price = open("main/banco_de_dados/price.txt", "a")
+    preçoproduto = float(input("Product's price: "))
+    price.write(transfstr(preçoproduto)) and price.write("\n")
+    listprice.append(preçoproduto)
+    totprod += 1
 
 
+# Verifica se o produto tem desconto, se tiver, aplica o desconto
+    if preçoproduto >= 100:
+        totdesc += 1
+        preçoproduto = preçoproduto - desconto(preçoproduto)
+        total += preçoproduto
+    else:
+        total += preçoproduto
+    name.close()
+    price.close()
 
 
-
+# Pergunta se deseja adicionar mais produtos
+    decide = int(input("Add more products? [1] Yes [2] No: "))
+    if decide == 2:
+        print(f"Total products: {totprod}")
+        print(f"Total products on sale: {totdesc}")
+        print(f"Total to pay: {total}")
+        break
