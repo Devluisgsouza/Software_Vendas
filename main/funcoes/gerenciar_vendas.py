c1b9c9f7f3114bba46b1f18ca8ef1a4eb91fa4a5
DevLGSouza <devlgsouza@gmail.com>
2025-03-27 22:36:56 -0300

    atribuições

--- main/funcoes/gerenciar_vendas.py
+++ main/funcoes/gerenciar_vendas.py
@@ -47,122 +47,87 @@ def alterar_venda():
         for linha in range(2,ultima_linha + 1):
             id_venda = banco_vendas.cell(row=linha, column=1).value
             if id == id_venda:
-                print(f"{"DO YOU WANT TO CHANGE:  ".center(45)}{"\n"}")
-                print("[1] PRODUCT NAME ".center(39))
-                print("[2] PRODUCT PRICE ".center(39))
-                print("[3] DATE OF SALE ".center(39))
-                print("[4] RETURN TO MENU ".center(41))
-                resp = input("\n").strip()
-                match resp:
-                    case "1":
-                        while True:
-                            new_name = input(f"{"\033[1;36mENTER WITH THE NEW NAME: \033[1;32m"}").strip().lower()
-                            if new_name == "":
-                                print(f"{"\n"}{"\033[1;31m ERROR! ENTER A VALID NAME \033[m".center(51)}{"\n"}")
-                            else:
-                                banco_vendas.cell(row=linha, column=2).value = new_name
-                                arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                                break
-                        while True:
-                            print(f"{"\n"}{"\033[1;36mWOULD YOU LIKE TO CHANGE MORE SALES?".center(53)}")
-                            resp = input(f"{'[1]YES   OR   [2]NO'.center(45)}{"\n"}").strip()
-                            if resp == "1":
-                                break
-                            elif resp == "2":
-                                return exit
-                            else:
-                                print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT \033[m".center(56)}")
-                                print(f"{"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2]".center(54)}{"\n"}")
-                    case "2":
-                        while True:
-                            pricestr = input(f"{"\033[1;36mENTER WITH THE NEW PRICE: \033[1;32m"}").strip().lower()
-                            try:
-                                new_price = float(pricestr)
-                                banco_vendas.cell(row=linha, column=3).value = new_price
-                                arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                                break
-                            except:
-                                print(f"{"\n"}{"\033[1;31m ERROR! ENTER A VALID PRICE \033[m".center(52)}")
-                                print(f"{"\033[1;31m USE ONLY THE “,” OR “.” TO ADD CENTS \033[m".center(52)}")
-                                print(f"{"\n"}{"\033[1;31m FOR EXAMPLE:\033[m".center(52)}")
-                                print(f"{"\033[1;32mR$0000,00 or R$0000.00\033[m".center(51)}{"\n"}")
-                        while True:
-                            print(f"{"\n"}{"\033[1;36mWOULD YOU LIKE TO CHANGE MORE SALES?".center(53)}")
-                            resp = input(f"{'[1]YES   OR   [2]NO'.center(45)}{"\n"}").strip()
-                            if resp == "1":
-                                break
-                            elif resp == "2":
-                                return exit
-                            else:
-                                print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT \033[m".center(56)}")
-                                print(f"{"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2]".center(54)}{"\n"}")
-                    case "3":
-                        while True:
-                            date = input(f"{"\033[1;36mENTER WITH THE NEW DATE: \033[1;32m"}").strip().lower()
-                            if date == "":
-                                print(f"{"\n"}{"\033[1;31m ERROR! ENTER A VALID DATE \033[m".center(51)}{"\n"}")
-                            else:
-                                banco_vendas.cell(row=linha, column=4).value = date
-                                arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                                break                              
+                while True:
+                    print(f"{"\n"}{"\033[1;36mDO YOU WANT TO CHANGE:  ".center(53)}{"\n"}")
+                    print("[1] PRODUCT NAME ".center(39))
+                    print("[2] PRODUCT PRICE ".center(39))
+                    print("[3] DATE OF SALE ".center(39))
+                    print("[4] RETURN TO MENU ".center(41))
+                    resp = input("\n").strip()
+                    match resp:
+                        case "1":
+                            while True:
+                                new_name = input(f"{"\033[1;36mENTER WITH THE NEW NAME: \033[1;32m"}").strip().lower()
+                                if new_name == "":
+                                    print(f"{"\n"}{"\033[1;31m ERROR! ENTER A VALID NAME \033[m".center(51)}{"\n"}")
+                                else:
+                                    banco_vendas.cell(row=linha, column=2).value = new_name
+                                    arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
+                                    break
+                            while True:
+                                print(f"{"\n"}{"\033[1;36mWOULD YOU LIKE TO CHANGE MORE SALES?".center(53)}")
+                                resp = input(f"{'[1]YES   OR   [2]NO'.center(45)}{"\n"}").strip()
+                                if resp == "1":
+                                    break
+                                elif resp == "2":
+                                    return exit
+                                else:
+                                    print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT \033[m".center(56)}")
+                                    print(f"{"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2]".center(54)}{"\n"}")
+                            break
+                        case "2":
+                            while True:
+                                pricestr = input(f"{"\033[1;36mENTER WITH THE NEW PRICE: \033[1;32m"}").strip().lower()
+                                try:
+                                    new_price = float(pricestr)
+                                    banco_vendas.cell(row=linha, column=3).value = new_price
+                                    arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
+                                    break
+                                except:
+                                    print(f"{"\n"}{"\033[1;31m ERROR! ENTER A VALID PRICE \033[m".center(52)}")
+                                    print(f"{"\033[1;31m USE ONLY THE “,” OR “.” TO ADD CENTS \033[m".center(52)}")
+                                    print(f"{"\n"}{"\033[1;31m FOR EXAMPLE:\033[m".center(52)}")
+                                    print(f"{"\033[1;32mR$0000,00 or R$0000.00\033[m".center(51)}{"\n"}")
+                            while True:
+                                print(f"{"\n"}{"\033[1;36mWOULD YOU LIKE TO CHANGE MORE SALES?".center(53)}")
+                                resp = input(f"{'[1]YES   OR   [2]NO'.center(45)}{"\n"}").strip()
+                                if resp == "1":
+                                    break
+                                elif resp == "2":
+                                    return exit
+                                else:
+                                    print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT \033[m".center(56)}")
+                                    print(f"{"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2]".center(54)}{"\n"}")
+                            break
+                        case "3":
+                            while True:
+                                print(f"{"\n"}{"\033[1;36mENTER WITH THE NEW DATE: \033[1;32m".center(59)}")
+                                print(f"{"\n"}{"FOR EXAMPLE: DD/MM/YYYY".center(44)}{"\n"}")
+                                date = input().replace("-","/").replace(".","/").replace(" ","/").strip()
+                                if date == "":
+                                    print(f"{"\033[1;31m ERROR! ENTER A VALID DATE \033[m".center(51)}{"\n"}")
+                                else:
+                                    banco_vendas.cell(row=linha, column=4).value = date
+                                    arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
+                                    break               
+                            while True:
+                                print(f"{"\n"}{"\033[1;36mWOULD YOU LIKE TO CHANGE MORE SALES?".center(53)}")
+                                resp = input(f"{'[1]YES   OR   [2]NO'.center(45)}{"\n"}").strip()
+                                if resp == "1":
+                                    break
+                                elif resp == "2":
+                                    return exit
+                                else:
+                                    print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT \033[m".center(56)}")
+                                    print(f"{"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2]".center(54)}{"\n"}")
+                            break
+                        case "4":
+                            return exit   
+                        case _:
+                            print(f"{"\n"}{"\033[1;31m I CAN'T UNDERSTAND WHAT YOU WANT! ".center(50)}")
+                            print(f"\033[1;31mCHOOSE THE OPTION USING THE NUMBERS [1,2,3,4]\033[m]".center(47))                                                            
+            
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # print(f"{"\n"}{"\033[1;31m SALE NOT FOUND \033[m".center(52)}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    #             nome = input(f"{"ENTER WITH THE NEW NAME: "}")
-                    #             nome_venda = banco_vendas.cell(row=linha, column=2).value
-                                
-                    #             arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                    # case "1":
-                    #     preço = input(f"{"ENTER WITH THE NEW PRICE: "}")
-                    #     preço_venda = preço
-                    #     arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                    # case "3":
-                    #     data = input(f"{"ENTER WITH THE NEW DATE: "}")
-                    #     data_venda = data
-                    #     arquivo_bancodd.save("main\\banco_de_dados\\banco.xlsx")
-                    # case "4":
-                    #     return exit
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 alterar_venda()
 # exlcuir_ven
